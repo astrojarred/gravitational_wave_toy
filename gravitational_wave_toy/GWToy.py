6a15dd79409bf210410bc2b4ae1e1823e280e193
Jarred Green <astrojarred@gmail.com>
2021-02-12 18:47:39 +0000

    Update methods

--- gravitational_wave_toy/GWToy.py
+++ gravitational_wave_toy/GWToy.py
@@ -47,6 +47,13 @@ def ParseGrbsens(catFileName, Separator="\t", names=None, orient="list"):
     return as_dict
 
 
+def open_grbsens(filename: str):
+    col_names = ["obs_time", "crab_flux", "photon_flux", "energy_flux", "sensitivity"]
+    sensi_list = ParseGrbsens(filename, names=col_names, orient="list")
+
+    return sensi_list
+
+
 def ParseBNS(filename, sep=" ", names=None, orient="index"):
     as_dict = pd.read_csv(
         filename, sep=sep, quotechar='"', comment="#", names=names
@@ -56,9 +63,7 @@ def ParseBNS(filename, sep=" ", names=None, orient="index"):
 
 # open grbsens files and create interpolation class:
 def get_interpolation(filename: str):
-    col_names = ["obs_time", "crab_flux", "photon_flux", "energy_flux", "sensitivity"]
-    sensi_list = ParseGrbsens(filename, names=col_names, orient="list")
-
+    sensi_list = open_grbsens(filename)
     # interpolation, x-> obstime, y-> photon_flux
     interpolation = interp1d(sensi_list["obs_time"], sensi_list["photon_flux"])
 
@@ -66,7 +71,6 @@ def get_interpolation(filename: str):
 
 
 def get_interpolation_dict(file_dict: dict):
-
     for direction, zeniths in file_dict.items():
         for zenith, file in zeniths.items():
             file_dict[direction][zenith] = get_interpolation(
@@ -84,8 +88,48 @@ def interpolate_grbsens(x: float, direction: str, zenith: int, interpoations: di
         raise AttributeError(f"No {direction} z{zenith} file found!")
 
 
-def get_energy_limits(zenith: int):
+def fit_grbsens(filepath: str):
+    grbsens = open_grbsens(filepath)
 
+    result = scipy.stats.linregress(np.log10(grbsens["obs_time"]), np.log10(grbsens["photon_flux"]))
+
+    return result
+
+
+def get_fit_dict(file_dict: dict):
+    output = {}
+
+    for direction, zeniths in file_dict.items():
+        output[direction] = {}
+        for zenith, file in zeniths.items():
+            # get fit and write results
+            result = fit_grbsens(file)
+            output[direction][zenith] = result
+
+    return output
+
+
+def fit(t: float, fit_dict: dict, site: str, zenith: int):
+    if zenith not in [20, 40, 60]:
+        raise AttributeError("Zenith must be 20, 40, or 60.")
+
+    if site.lower() not in ["south", "north"]:
+        raise AttributeError("Site must be `south` or `north`.")
+
+    result = fit_dict[site.lower()][zenith]
+
+    # catch really low numbers
+    if t < 0.1:
+        t = 0.1
+
+    return 10 ** (result.slope * np.log10(t) + result.intercept)
+
+
+def fit_compact(t: float, slope: float, intercept: float):
+    return 10 ** (slope * np.log10(t) + intercept)
+
+
+def get_energy_limits(zenith: int):
     if zenith not in [20, 40, 60]:
         raise AttributeError("Zenith must be 20, 40, or 60.")
     if zenith == 20:
@@ -125,7 +169,7 @@ def find_files(catalog_directory: str, ext: str = "fits"):
 
 def open_v1_fits(filepath: str):
     try:
-        
+
         grb = {}
 
         with fits.open(filepath) as hdu_list:
@@ -136,7 +180,7 @@ def open_v1_fits(filepath: str):
             grb["dec"] = hdu_list[0].header["DEC"]
             grb["eiso"] = hdu_list[0].header["EISO"]
             grb["z"] = hdu_list[0].header["REDSHIFT"]
-            grb["ange"] = hdu_list[0].header["ANGLE"]
+            grb["angle"] = hdu_list[0].header["ANGLE"]
 
             datalc = hdu_list[3].data
             datatime = hdu_list[2].data
@@ -152,95 +196,157 @@ def open_v1_fits(filepath: str):
     except FileNotFoundError:
         print(f"Input V1 GRB {filepath} not found.")
 
+
 # Spectrum
 def spectrum(x):
     return (x / 1) ** (-2.1)
 
-def observe_grb(bns_index: int, bns_dict: dict, interpolations, inttime: float, tstart: float = 0, observatory=None):
+
+def get_integral_spectra(zeniths: list):
+    output = {}
+
+    for z in zeniths:
+        output[z] = {}
+
+        lower, upper = get_energy_limits(z)
+
+        intl, errl = integrate.quad(lambda x: spectrum(x), lower, upper)
+
+        output[z]["integral"] = intl
+        output[z]["error"] = errl
+
+    return output
+
+
+def _to_iterator(obj_ids):
+    """To decide when to update tqdm bar
+    See: https://github.com/ray-project/ray/issues/5554#issuecomment-558397627"""
+
+    while obj_ids:
+        done, obj_ids = ray.wait(obj_ids)
+        try:
+            yield ray.get(done[0])
+        except RuntimeError:
+            yield 1
+
+
+def observe_grb(bns_index: int, bns_dict: dict, fit_dict: dict, integral_dict: dict, tstart: float = 0,
+                     max_time=None, precision=1, observatory=None, zenith=None):
+    """Modified version to increase timestep along with time size"""
 
     # get run and merger ID
 
-    run = bns[bns_index]["run"]
-    merger_id = bns[bns_index]["MergerID"]
+    run = bns_dict[bns_index]["run"]
+    merger_id = bns_dict[bns_index]["MergerID"]
     if "merger" in merger_id.lower():
         merger_id = merger_id[6:]
 
-    
-
-    info = {
-        "run": bns[bns_index]["run"],
-        "MergerID": merger_id,
-        "zenith": bns[bns_index]["Mean Altitude"],
-        "observatory": bns[bns_index]["Observatory"],
-        "tstart": 0,
-        "tend": 0,
-        "obstime": 0,
-        "seen": False,
-    }
-
-    InputGRB = f"GammaCatalogV1.0/{run}_{merger_id}.fits"
+    InputGRB = f"./GammaCatalogV1.0/{run}_{merger_id}.fits"
 
     # open grb file
     grb = open_v1_fits(InputGRB)
 
     # add other bns information:
     #    - later these can be replaces with functions to calculate them
-    grb["zenith"] = bns[bns_index]["Mean Altitude"]
-    grb["observatory"] = bns[bns_index]["Observatory"]
+    grb["zenith"] = bns_dict[bns_index]["Mean Altitude"]
+    grb["observatory"] = bns_dict[bns_index]["Observatory"]
     if observatory:
         # force observatory if given as input
-        grb["observatory"] = observatory.lower().capitalize()
+        grb["observatory"] = observatory.lower()
+    if zenith:
+        # force zenith if given as input
+        grb["zenith"] = zenith
+
+    z = grb["zenith"]
+    site = grb["observatory"].lower()
 
     # set the grb to not be seen by default
     grb["seen"] = False
+    grb["tend"] = -1
+    grb["obstime"] = -1
 
-    # Integral of the spectrum
+    # Integral of the spectra
 
-    lower, upper = get_energy_limits(grb['zenith'])
+    integral_spectrum = integral_dict[z]["integral"]  # GeV
 
-    intl, errl = integrate.quad(lambda x: spectrum(x), lower, upper)  # GeV
+    sens_slope = fit_dict[site][z].slope
+    sens_intercept = fit_dict[site][z].intercept
 
-    # Interpolation of the flux with time
-    flux = interp1d(grb["time"], grb["lc"])
+    # set default max time
+    if max_time is None:
+        max_time = 86400 + tstart  # 24h after starting observations
+    else:
+        max_time += tstart
 
     # start the procedure
     dt = 1
     grb["tstart"] = tstart
 
-    for m in range(1, inttime):  # second loop from 1 to max integration time
+    # Interpolation of the flux with time
+    flux = interp1d(grb["time"], grb["lc"], fill_value="extrapolate")
 
-            t = tstart + m * dt  # tstart = 210, + loop number
-            obst = t - tstart  # how much actual observing time has gone by
+    n = 1  # keeps track of loops carried out at current scale
+    previous_dt = -1
+    original_tstart = tstart
+    t = tstart + n
+    previous_t = t
+    autostep = True
 
-            fluencen, errorn = integrate.quad(lambda x: flux(x), tstart, t)
-            averagefluxn = fluencen * intl / obst  # ph/cm2/s
+    while t < max_time:  # second loop from 1 to max integration time
 
-            photon_flux = interpolate_grbsens(obst, grb["observatory"], grb["zenith"], interpolations)
+        if autostep:
 
-            if averagefluxn > photon_flux:  # if it is visible:
+            dt = 10 ** int(np.floor(np.log10(t)))
 
-                if tstart + obst < inttime:  # if starting time < max time, write
-                    tend = tstart + obst
-                    grb["tend"] = tend
-                    grb["obstime"] = obst
-                    grb["seen"] = True
+            if dt != previous_dt:  # if changing scale, reset n
+                n = 1
+
+        t = tstart + n * dt  # tstart = 210, + loop number
+        obst = t - original_tstart  # how much actual observing time has gone by
+
+        fluence = integrate.quad(flux, original_tstart, t)
+        avg_flux = fluence[0] * integral_spectrum / obst
+
+        # calculate photon flux
+        photon_flux = fit_compact(obst, sens_slope, sens_intercept)
+
+        # print(f"t={t:.2f}, dt={dt}, avgflux={avg_flux}, photon_flux={photon_flux}")
+
+        if avg_flux > photon_flux:  # if it is visible:
+            # print(f"\nClose solution, t={round(t, precision)}, avgflux={avg_flux}, photon_flux={photon_flux}")
+
+            if dt > 10 ** (-1 * precision):
+
+                # if the desired precision is not reached, go deeper
+                # change scale of dt and go back a step
+                autostep = False
+                tstart = previous_t
+                dt = dt / 10
+                n = 1
+
+            else:
+                # desired prevision is reached! Give solution
+
+                tend = original_tstart + obst
+                grb["tend"] = tend
+                grb["obstime"] = obst
+                grb["seen"] = True
 
                 tstart = tstart + obst
 
                 break
 
+        else:
+            # if not seen yet, keep going
+
+            previous_dt = dt
+            previous_t = t
+            n = n + 0.1
+
     # print(f"Seen: {new_row['seen']}")
-    return grb
+    del grb["lc"], grb["time"], grb["energy"], grb["spec"]
+    return pd.DataFrame(grb, index=[bns_index])
 
-    
-#######################################################
-
-# Get Script name
-ScriptName = os.path.split(sys.argv[0])[1].split(".")[0]
-
-#######################################################
-# Main
-#######################################################
 
 if __name__ == "__main__":
 
