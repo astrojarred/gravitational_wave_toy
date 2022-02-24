4313d5e7e2d12923fcfda622ce7b4c4f79bfb148
Jarred Green <astrojarred@gmail.com>
2022-02-24 17:12:28 +0100

    Remove excess logging

--- gravitational_wave_toy/GWToyV3.py
+++ gravitational_wave_toy/GWToyV3.py
@@ -33,27 +33,7 @@ from tqdm.auto import tqdm
 
 
 # Set up logging!
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-    datefmt="%m-%d %H:%M",
-    filename="./main_log.log",
-    filemode="a",
-)
-
-# define a Handler which writes INFO messages or higher to the sys.stderr
-console = logging.StreamHandler()
-console.name = "Console"
-console.setLevel(logging.INFO)
-# set a format which is simpler for console use
-formatter = logging.Formatter("%(name)-12s: %(levelname)-8s %(message)s")
-# tell the handler to use this format
-console.setFormatter(formatter)
-# add the handler to the root logger
-logger.addHandler(console)
+logging.basicConfig(level=logging.INFO)
 
 # classes
 class Sensitivities:
