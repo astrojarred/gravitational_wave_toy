251af0335708f6ae81b40e4df7c4cd6588e29cef
Jarred Green <astrojarred@gmail.com>
2022-02-26 13:21:55 +0100

    Update v3 to use Dask

--- gravitational_wave_toy/GWToyV3.py
+++ gravitational_wave_toy/GWToyV3.py
@@ -10,7 +10,7 @@
 # imports
 import warnings
 
-warnings.filterwarnings("ignore")  # surpress warnings when using on the command line
+# warnings.filterwarnings("ignore")  # surpress warnings when using on the command line
 
 import glob
 import logging
@@ -21,15 +21,21 @@ import numpy as np
 warnings.simplefilter("ignore", np.RankWarning)
 
 import pandas as pd
-import ray
+# import ray
 import scipy
 import scipy.stats
 import yaml
 from astropy.io import fits
 from scipy import integrate
-from scipy.interpolate import interp1d, RectBivariateSpline, RegularGridInterpolator
+from scipy.interpolate import interp1d, RegularGridInterpolator
 from matplotlib import pyplot as plt
 from tqdm.auto import tqdm
+from tqdm.dask import TqdmCallback
+
+import dask
+import dask.bag as db
+# from dask.diagnostics import ProgressBar
+# from dask.distributed import Client, progress
 
 
 # Set up logging!
@@ -108,7 +114,6 @@ class GRB:
         energy_limits=[30, 10000],
     ) -> None:
 
-
         self.filepath = filepath
         self.zenith = 0
         self.site = "south"
@@ -135,7 +140,9 @@ class GRB:
             self.time = datatime.field(0)
             self.energy = dataenergy.field(0)
 
-            self.spectra = np.array([datalc.field(i) for i, e in enumerate(self.energy)])
+            self.spectra = np.array(
+                [datalc.field(i) for i, e in enumerate(self.energy)]
+            )
 
         # set site and zenith
         self.rng = np.random.default_rng(
@@ -144,10 +151,10 @@ class GRB:
 
         self.site = self.rng.choice(sites)
         self.zenith = self.rng.choice(zeniths)
-        
+
         # set spectral grid
         self.set_spectral_grid()
-        
+
         # fit spectral indices
         self.fit_spectral_indices()
 
@@ -155,10 +162,14 @@ class GRB:
             f"Got GRB run{self.run}_ID{self.id}, {self.site}, z{self.zenith}, {self.angle}º"
         )
 
+    def __repr__(self):
+        return f"<GRB(run={self.run}, id={self.id})>"
 
     def set_spectral_grid(self):
 
-        self.SpectralGrid = RegularGridInterpolator((np.log10(self.energy), np.log10(self.time)), self.spectra)
+        self.SpectralGrid = RegularGridInterpolator(
+            (np.log10(self.energy), np.log10(self.time)), self.spectra
+        )
 
     def show_spectral_pattern(self, resolution=100):
 
@@ -167,8 +178,8 @@ class GRB:
         loge = np.around(np.log10(self.energy), 1)
         logt = np.around(np.log10(self.time), 1)
 
-        x = np.around(np.linspace(min(loge), max(loge), resolution+1), 1)
-        y = np.around(np.linspace(min(logt), max(logt), resolution+1), 1)
+        x = np.around(np.linspace(min(loge), max(loge), resolution + 1), 1)
+        y = np.around(np.linspace(min(logt), max(logt), resolution + 1), 1)
 
         points = []
         for e in x:
@@ -177,8 +188,11 @@ class GRB:
 
         plt.xlabel("Log(t)")
         plt.ylabel("Log(E)")
-        plt.imshow(np.log10(self.SpectralGrid(points)).reshape(resolution+1, resolution+1), extent=(0,1,0,1), cmap="viridis")
-
+        plt.imshow(
+            np.log10(self.SpectralGrid(points)).reshape(resolution + 1, resolution + 1),
+            extent=(0, 1, 0, 1),
+            cmap="viridis",
+        )
 
     def get_spectrum(self, time, energy=None):
 
@@ -186,7 +200,9 @@ class GRB:
             energy = self.energy
 
         if hasattr(energy, "__len__"):
-            return np.array([self.SpectralGrid((e, np.log10(time))) for e in np.log10(energy)])
+            return np.array(
+                [self.SpectralGrid((e, np.log10(time))) for e in np.log10(energy)]
+            )
         else:
             return self.SpectralGrid((np.log10(energy), np.log10(time)))
 
@@ -196,11 +212,11 @@ class GRB:
             time = self.time
 
         if hasattr(time, "__len__"):
-            return np.array([self.SpectralGrid((np.log10(energy), t)) for t in np.log10(time)])
+            return np.array(
+                [self.SpectralGrid((np.log10(energy), t)) for t in np.log10(time)]
+            )
         else:
             return self.SpectralGrid((np.log10(energy), np.log10(time)))
-        
-        
 
     def fit_spectral_indices(self):
 
@@ -217,7 +233,11 @@ class GRB:
             if len(idx[idx] > 3):  # need at least 3 points in the spectrum to fit
 
                 times.append(time)
-                indices.append(np.polyfit(np.log10(self.energy[idx]), np.log10(spectrum[idx]), 1)[0])
+                indices.append(
+                    np.polyfit(np.log10(self.energy[idx]), np.log10(spectrum[idx]), 1)[
+                        0
+                    ]
+                )
             else:
                 bad_times.append(time)
 
@@ -230,25 +250,25 @@ class GRB:
             self._indices,
             fill_value="extrapolate",
         )
-        
+
     def get_spectral_index(self, time):
-        
+
         return self.index_at(np.array([np.log10(time)]))[0]
 
-
     def show_spectral_evolution(self, resolution=100):
-        
+
         self.fit_spectral_indices()
-        
-        t = np.linspace(np.log10(min(self.time)), np.log10(max(self.time)), resolution+1)
-        
+
+        t = np.linspace(
+            np.log10(min(self.time)), np.log10(max(self.time)), resolution + 1
+        )
+
         plt.plot(t, self.index_at(t))
         plt.xlabel("Log(t) (s)")
         plt.ylabel("Spectral Index")
-        
-        
+
         plt.show()
-        
+
     def get_integral_spectrum(self, time, first_energy_bin):
 
         spectral_index = self.get_spectral_index(time)
@@ -264,7 +284,7 @@ class GRB:
         )
 
         return integral_spectrum
-    
+
     def get_fluence(self, start_time, stop_time):
 
         first_energy_bin = min(self.energy)
@@ -277,17 +297,21 @@ class GRB:
 
         logging.debug(f"    Fluence: {fluence}")
         return fluence
-    
+
     def output(self):
 
         keys_to_drop = [
             "time",
             "energy",
             "spectra",
-            "spectrum",
+            "SpectralGrid",
             "rng",
             "power_law_slopes",
             "spectral_indices",
+            "_indices",
+            "_index_times",
+            "_bad_index_times",
+            "index_at",
         ]
         return {
             key: value
@@ -311,9 +335,7 @@ def _to_iterator(obj_ids):
 def check_if_visible(grb: GRB, sensitivity: Sensitivities, start_time, stop_time):
 
     # Interpolation and integration of the flux with time
-    average_flux = grb.get_fluence(start_time, stop_time) / (
-        stop_time - start_time
-    )
+    average_flux = grb.get_fluence(start_time, stop_time) / (stop_time - start_time)
 
     # calculate photon flux
     photon_flux = sensitivity.get(
@@ -381,7 +403,7 @@ def observe_grb(
     if read:
         if Path(log_filename).exists():
             # logging.debug(f"Output already exists: {log_filename}")
-            
+
             # return pd.read_csv(log_filename, index_col=0)
             return log_filename
 
@@ -465,6 +487,116 @@ def observe_grb(
     return log_filename
 
 
+def observe_grb_parallel(params):
+    """Modified version to increase timestep along with time size"""
+
+    grb_file_path = params[0]
+    sensitivity = params[1]
+    log_directory = params[2]
+    start_time = params[3]
+    zeniths = params[4]
+    sites = params[5]
+    random_seed = params[6]
+    target_precision = params[7]
+    max_time = None
+    energy_limits = [30, 10000]
+    read = False
+
+    grb = GRB(
+        grb_file_path,
+        random_seed=random_seed,
+        zeniths=zeniths,
+        sites=sites,
+        energy_limits=energy_limits,
+    )
+
+    try:
+
+        # get energy limits
+        grb.min_energy, grb.max_energy = sensitivity.get_energy_limits(
+            grb.site, grb.zenith
+        )
+
+        # start the procedure
+        grb.start_time = start_time
+        delay = start_time
+
+        # set default max time
+        if max_time is None:
+            max_time = 43200  # 12h after starting observations
+
+        # check maximum time
+        visible = check_if_visible(grb, sensitivity, delay, max_time + delay)
+
+        # not visible even after maximum observation time
+        if not visible:
+
+            return grb.output()
+
+        loop_number = 0
+        precision = int(10 ** int(np.floor(np.log10(max_time + delay))))
+        observation_time = precision
+        previous_observation_time = precision
+
+        # find the inflection point
+        while loop_number < 10000:
+
+            loop_number += 1
+            visible = check_if_visible(
+                grb, sensitivity, delay, delay + observation_time
+            )
+
+            if visible:
+
+                # if desired precision is reached, return results and break!
+                if np.log10(precision) == np.log10(target_precision):
+                    round_precision = int(-np.log10(precision))
+                    end_time = delay + round(observation_time, round_precision)
+                    grb.end_time = round(end_time, round_precision)
+                    grb.obs_time = round(observation_time, round_precision)
+                    grb.seen = True
+                    # logging.debug(f"    obs_time={observation_time} end_time={end_time}")
+                    break
+
+                elif observation_time == precision:
+                    # reduce precision
+                    precision = 10 ** (int(np.log10(precision)) - 1)
+                    observation_time = precision
+                    # logging.debug(f"    Updating precision to {precision}")
+
+                else:  # reduce precision but add more time
+                    precision = 10 ** (int(np.log10(precision)) - 1)
+                    observation_time = previous_observation_time + precision
+                    # logging.debug(
+                    #     f"    Going back to {previous_observation_time} and adding more time {precision}s"
+                    # )
+
+            else:
+                previous_observation_time = observation_time
+                observation_time += precision
+                # update DT and loop again
+
+        # just return dict now
+        return grb.output()
+
+    except Exception as e:
+        print("BAD NEWS!")
+        print(e)
+
+        grb.seen = "error"
+
+        return grb.output()
+
+
+def batch_observe_grb(batch):
+    batch_results = []
+    for grb_params in batch:
+        res = observe_grb_parallel(grb_params)
+        batch_results.append(res)
+
+    return batch_results
+
+
 def run():
 
     logging.info("Welcome to GWToy for CTA, for use with catalogue v1.")
@@ -500,7 +632,7 @@ def run():
     if not log_directory:
         log_directory = "./gw_toy_logs"
     Path(log_directory).mkdir(parents=True, exist_ok=True)
-    Path(log_directory+"/logs").mkdir(parents=True, exist_ok=True)
+    Path(log_directory + "/logs").mkdir(parents=True, exist_ok=True)
 
     if not precision:
         precision = 2
@@ -534,61 +666,57 @@ def run():
 
     # initialize ray and create remote solver
     logging.info("Starting ray:")
-    ray.init(num_cpus=n_cores, log_to_driver=False, logging_level=logging.FATAL)
-    observe_grb_remote = ray.remote(observe_grb)
 
     total_runs = n_grbs * len(time_delays)
 
     logging.info(f"Running {total_runs} observations")
-    # set up each observation
-    grb_object_ids = [
-        observe_grb_remote.remote(
-            grb_file_path,
-            sensitivity,
-            log_directory=log_directory,
-            start_time=delay,
-            zeniths=zeniths,
-            sites=sites,
-            random_seed=random_seed,
-            target_precision=precision,
-        )
-        for grb_file_path in file_list[first_index:last_index]
-        for delay in sorted(time_delays, reverse=True)
-    ]
 
-    for _ in tqdm(_to_iterator(grb_object_ids), total=total_runs):
-        pass
+
+    # set up input parameters
+    input_params = []
+
+    with tqdm(total=total_runs, desc="Preparing input parameters") as pbar:
+        for grb_file_path in file_list[first_index:last_index]:
+            for delay in time_delays:
+                input_params.append([
+                    grb_file_path,
+                    sensitivity,
+                    log_directory,
+                    delay,
+                    zeniths,  
+                    sites, 
+                    random_seed,
+                    precision,
+                ])
+                pbar.update(1)
+
+    # set up bags
+    npartitions = int(np.ceil(total_runs / 40))
+    logging.info(f"Setting up ~{npartitions} Dask Partitions. -> ~40 runs in each.")
+
+    b = db.from_sequence(
+        input_params, 
+        npartitions=input_params
+    )
+
+    b = b.map_partitions(batch_observe_grb)
+
+    logging.info("OK! Running simulations.")
+    with TqdmCallback(desc="compute"):
+        results_bag = b.compute()
+    logging.info("Done!")
 
     # run the observations
-    logging.info("Done observing!\nCollecting csv filenames.")
-    csvs = []
-    for obj_id in tqdm(grb_object_ids, total=total_runs):
-        this_result = ray.get(obj_id)
-        if not isinstance(this_result, type(None)):
-            csvs.append(this_result)
+    logging.info("Saving results.")
+    results = pd.DataFrame(results_bag)
 
-    logging.info("Done. Shutting down Ray.")
-    ray.shutdown()
-
-    logging.info("Creating the combined output")
-
-    # create the final pandas dataframe and write to a csv
-    dfs = []
-    for filename in tqdm(csvs, total=total_runs):
-        try:
-            df = pd.read_csv(filename, index_col=0)
-            dfs.append(df)
-        except (pd.errors.EmptyDataError, TypeError) as e:
-            pass
-
-    final_table = pd.concat(dfs, axis=0)
-
-    logging.info("Saving files. ")
-    final_table.to_csv(output_filename, index=False)
+    results.to_csv(output_filename, index=False)
     logging.info(f"Saved csv: {output_filename}")
     pickle_filename = output_filename.split(".")[0] + ".pkl"
-    final_table.to_pickle(pickle_filename)
+    results.to_pickle(pickle_filename)
     logging.info(f"Saved pandas dataframe: {pickle_filename}")
+
+    logging.info("Done. Shutting down Ray.")
 
     logging.info("All done!")
 
