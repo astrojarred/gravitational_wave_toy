b3a8db6151f4ad6e0280602334a70fd48f360cd3
Jarred Green <astrojarred@gmail.com>
2025-11-12 15:07:54 +0100

    add logging tests and docstrings

--- gravitational_wave_toy/logging.py
+++ gravitational_wave_toy/logging.py
@@ -2,6 +2,32 @@ import logging
 
 
 class logger:
+    """
+    A logger for the package. Will automatically create a log file in the current working directory, and print to the console.
+
+    Attributes:
+        name (str): The name of the logger.
+        filename (str): The filename of the log file.
+        level (int): The level of the logger.
+        file_level (int): The level of the log file.
+
+    Methods:
+        debug(self, message: str) -> None: Log a debug message.
+        info(self, message: str) -> None: Log an info message.
+        warning(self, message: str) -> None: Log a warning message.
+        error(self, message: str) -> None: Log an error message.
+        critical(self, message: str) -> None: Log a critical message.
+
+    Example:
+        >>> from gravitational_wave_toy.logging import logger
+        >>> log = logger(name='test', filename='./gwtoy.log')
+        >>> log.debug('This is a debug message')
+        >>> log.info('This is an info message')
+        >>> log.warning('This is a warning message')
+        >>> log.error('This is an error message')
+        >>> log.critical('This is a critical message')
+    """
+
     def __init__(
         self,
         name: str,
@@ -51,15 +77,3 @@ class logger:
 
     def critical(self, message: str) -> None:
         self.logger.critical(message)
-
-
-"""python
-# example usage
-from gravitational_wave_toy.logging import logger
-log = logger(name='test', filename='./gwtoy.log')
-log.debug('This is a debug message')
-log.info('This is an info message')
-log.warning('This is a warning message')
-log.error('This is an error message')
-log.critical('This is a critical message')
-"""
